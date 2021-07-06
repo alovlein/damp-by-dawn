698c33d1a56f9cdeb766299378e65e53a47b00f2
Alec Lovlein <alovlein@gmail.com>
2021-07-06 15:42:17 -0500

    getting website started

--- web/main/models.py
+++ web/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Sensors(models.Model):
+    
+    name = models.CharField(max_length=32)
+    relative_coor_north = models.FloatField()
+    relative_coor_west = models.FloatField()
+    read_date = models.DateTimeField()
+    read_value = models.FloatField()
+
+
+    class Meta:
+        ordering = ['-name']
+
+
+    def __str__(self):
+        return self.name
+
+
--- web/main/views.py
+++ web/main/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Sensors
 
 def index(request):
-    return HttpResponse('This is the main index')
+    return HttpResponse('This is the main index and live editing still works')
+
+def sensor_current_status(request, sensor_id):
+    sensor_statuses = Sensors.objects.filter(name__startswith=sensor_id).order_by('name')
+    context = {'sensor_statuses': sensor_statuses}
+    return render(request, 'main/sensor_status.html', context)
+
+def sensor_history(request, sensor_id):
+    return HttpResponse(f'This houses the historical sensor readings graphs for sensor {sensor_id}')
+
 
