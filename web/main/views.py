55bc1986ccb7c4142badc94f894a7894e3415aa1
Alec Lovlein <alovlein@gmail.com>
2021-07-06 16:20:19 -0500

    added basic plot function

--- web/main/views.py
+++ web/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Sensors
+from .utils import plot
 
 def index(request):
     return HttpResponse('This is the main index and live editing still works')
@@ -11,6 +12,11 @@ def sensor_current_status(request, sensor_id):
     return render(request, 'main/sensor_status.html', context)
 
 def sensor_history(request, sensor_id):
-    return HttpResponse(f'This houses the historical sensor readings graphs for sensor {sensor_id}')
+    return graph_view_example(request, sensor_id)
+
+def graph_view_example(request, sensor_id):
+    example_graph = plot.graph_example()
+    context = { 'graph': example_graph }
+    return render(request, 'main/graph_example.html', context)
 
 
